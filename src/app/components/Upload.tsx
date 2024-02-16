a60433ed1b838829c044b5f49eb896ff9e6f2c30
Matt Bremmers <bremmdev@gmail.com>
2024-02-16 23:04:03 +0100

    allow multiple files

--- src/app/components/Upload.tsx
+++ src/app/components/Upload.tsx
@@ -3,31 +3,57 @@
 import React from "react";
 import { uploadToS3 } from "@/app/utils/upload";
 
-const Upload = () => {
+type Props = {
+  maxFileCount?: number;
+};
+
+const Upload = (props: Props) => {
+  const { maxFileCount = 1 } = props;
+
+  const [error, setError] = React.useState<string | null>(null);
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const file = (event.target as HTMLFormElement).file.files[0];
-    if (!file) return null;
+    setError(null);
+
+    const files = (event.target as HTMLFormElement).file.files as FileList;
+    if (!files || files.length === 0) return;
+
+    if (files.length > maxFileCount) {
+      setError(`Please select up to ${maxFileCount} files`);
+      return;
+    }
+
     try {
-      const key = await uploadToS3(file);
-      console.log(`File uploaded successfully with key: ${key}`);
+      const uploadPromises = Array.from(files).map((file) => uploadToS3(file));
+      const keys = await Promise.all(uploadPromises);
+      keys.forEach((key) =>
+        console.log(`File uploaded successfully with key: ${key}`)
+      );
     } catch (error) {
-      console.error("Error uploading file", error);
+      setError(error.message);
     }
   };
 
+  const allowMultiple = maxFileCount > 1;
+  const maxFileCountMessage = allowMultiple
+    ? `up to ${maxFileCount} files`
+    : "a file";
+
   return (
     <>
-      <p>Please select file to upload</p>
+      <p>Please select {maxFileCountMessage}</p>
       <form onSubmit={handleSubmit}>
         <input
           type="file"
           id="file"
           name="file"
           accept="image/*, video/*, audio/*"
+          multiple={allowMultiple}
         />
         <button type="submit">Upload</button>
       </form>
+      {error && <p className="text-red-500">{error}</p>}
     </>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Upload from "./components/Upload";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Upload />
+      <Upload maxFileCount={3} />
     </main>
   );
 }
